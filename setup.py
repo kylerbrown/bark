2dbf599acf38299d7568415bf43d795c556ec497
Dan Meliza <dan@meliza.org>
2013-07-15 15:56:52 -0500

    tick version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 if sys.hexversion < 0x02060000:
     raise RuntimeError("Python 2.6 or higher required")
 
-VERSION = '2.0.0'
+VERSION = '2.0.1'
 
 cls_txt = """
 Development Status :: 5 - Production/Stable
