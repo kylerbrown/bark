983b3bb160e0051d1d1a47a70fd30441c23773ad
Kyler Brown <kylerjbrown@gmail.com>
2016-11-28 15:58:46 -0600

    adding splits to data stream

--- bark/datutils.py
+++ bark/datutils.py
@@ -1,13 +1,10 @@
 import numpy as np
 import bark
 
+
 class Stream():
-    def __init__(self, data,
-            sr,
-            attrs=None,
-            chunksize=1e6,
-            ):
-        self.data = data     
+    def __init__(self, data, sr, attrs=None, chunksize=1e6, ):
+        self.data = data
         self.sr = sr
         if attrs:
             self.attrs = attrs.copy()
@@ -16,17 +13,14 @@ class Stream():
         self.chunksize = int(chunksize)
         self.index = 0
 
-
     def __iter__(self):
         return self
-
 
     def __next__(self):
         if hasattr(self.data, "__next__"):
             return next(self.data)
         try:
-            result = self.data[self.index: self.index
-                    + self.chunksize]
+            result = self.data[self.index:self.index + self.chunksize]
         except IndexError:
             raise StopIteration
         print(result)
@@ -36,43 +30,37 @@ class Stream():
         print(self.index)
         return result
 
-
     def __add__(self, other):
-        return self._binary_operator(other,
-                lambda x,y : x + y)
+        return self._binary_operator(other, lambda x, y: x + y)
+
     def __sub__(self, other):
-        return self._binary_operator(other,
-                lambda x,y : x - y)
+        return self._binary_operator(other, lambda x, y: x - y)
+
     def __mul__(self, other):
-        return self._binary_operator(other,
-                lambda x,y : x * y)
+        return self._binary_operator(other, lambda x, y: x * y)
+
     def __truediv__(self, other):
-        return self._binary_operator(other,
-                lambda x,y : x / y)
+        return self._binary_operator(other, lambda x, y: x / y)
+
     def __floordiv__(self, other):
-        return self._binary_operator(other,
-                lambda x,y : x // y)
+        return self._binary_operator(other, lambda x, y: x // y)
 
     def new_stream(self, newdata):
         " Creates a new stream with new data"
-        return Stream(newdata, sr=self.sr,
-                attrs=self.attrs,
-                chunksize=self.chunksize)
-
+        return Stream(newdata,
+                      sr=self.sr,
+                      attrs=self.attrs,
+                      chunksize=self.chunksize)
 
     def _binary_operator(self, other, func):
         if isinstance(other, Stream):
-           return self.new_stream( (func(x, y)
-               for x,y in zip(self, other)))
+            return self.new_stream((func(x, y) for x, y in zip(self, other)))
         else:
-            return self.new_stream((func(x, other)
-                for x in self))
-
+            return self.new_stream((func(x, other) for x in self))
 
     def call(self):
         "Returns the data as a numpy array."
         return np.vstack(self)
-
 
     def write(self, filename, dtype=None):
         """ Saves to disk as raw binary """
@@ -87,7 +75,6 @@ class Stream():
         attrs["n_channels"] = data.shape[1]
         bark.write_metadata(filename + ".meta")
 
-
     def map(self, func, vectorize=False):
         """ Maps a function to data,
         intended for scalar operators like 
@@ -101,7 +88,10 @@ class Stream():
             func = np.vectorize(func)
         newdata = (func(x) for x in self)
         return self.new_stream(newdata)
-        
+
+    def __getitem__(self, key):
+        """ use python syntax for splitting columns out of the stream """
+        return self.new_stream((x[:, key]) for x in self)
 
     def vector_map(self, func):
         """
@@ -117,54 +107,83 @@ class Stream():
         """ helper function """
         N = self.chunksize // 3
         prev = None
-        try: 
+        try:
             for x in self:
                 if not prev:
                     y = func(x)
-                    yield y[: N,:]
+                    yield y[:N, :]
                     if len(y) > N:
-                        yield y[N: 2*N]
+                        yield y[N:2 * N]
                 else:
                     y = func(np.vstack((prev, x)))
-                    if len(y) > 2*N:
-                        yield y[2*N: 3*N]
-                    if len(y) > 3*N:
-                        yield y[3*N: 4*N]
-                    if len(y) > 4*N:
-                        yield y[4*N: 5*N]
+                    if len(y) > 2 * N:
+                        yield y[2 * N:3 * N]
+                    if len(y) > 3 * N:
+                        yield y[3 * N:4 * N]
+                    if len(y) > 4 * N:
+                        yield y[4 * N:5 * N]
                 prev = x
-            if len(y) > 5*N:
-                yield y[5*N:]
+            if len(y) > 5 * N:
+                yield y[5 * N:]
         except IndexError:
             raise StopIteration
-
-
 
     def _vector_map_old(self, func):
         """ helper function """
         x, x2, x3 = None, None, None
         try:
-            x1 = next(self) 
+            x1 = next(self)
             x = x1
             x2 = next(self)
-            x = np.vstack((x1,x2))
+            x = np.vstack((x1, x2))
             x3 = next(self)
-            x = np.vstack((x1,x2,x3))
+            x = np.vstack((x1, x2, x3))
         except StopIteration:
             pass
         if x1:
             yield func(x)[:len(x1), :]
         if x2:
-            yield fun(x)[len(x1): len(x1) + len(x2), :]
+            yield fun(x)[len(x1):len(x1) + len(x2), :]
 
         for data in self:
             x1 = x2
             x2 = x3
             x3 = data
             x = np.vstack(x1, x2, x3)
-            yield func(x)[len(x1):len(x)-len(x3), :]
+            yield func(x)[len(x1):len(x) - len(x3), :]
         if x3:
-            yield func(x)[len(x)-len(x3):, :]
+            yield func(x)[len(x) - len(x3):, :]
+
+    def split(self, *args):
+        return self.new_stream((x[:, args] for x in self))
+
+    def merge(*streams):
+        "Concatenate columns from streams"
+        return streams[0].new_stream((np.hstack(data) for data in zip(*streams)
+                                      ))
+
+    def chain(*streams):
+        return streams[0].new_stream((rechunk(data, self.chunksize)
+                                      for data in chain(*streams)))
+
+    def rechunk(self, chunksize):
+        self.chunksize = chunksize
+        return self.new_stream(rechunk(self, self.chunksize))
+
+
+def rechunk(stream, chunksize):
+    "New iterator with correct chunksize."
+    buffer = None
+    for x in stream:
+        if buffer is None:
+            buffer = x
+        else:
+            buffer = np.vstack((buffer, x))
+        while len(buffer) > chunksize:
+            print(len(buffer))
+            yield buffer[:chunksize, :]
+            buffer = buffer[chunksize:, :]
+    yield buffer  # leftover samples at end of stream
 
 
 class FileStream(Stream):
@@ -177,8 +196,7 @@ class FileStream(Stream):
 
     def __next__(self):
         try:
-            result = self.data[self.index: self.index
-                    + self.chunksize]
+            result = self.data[self.index:self.index + self.chunksize]
         except IndexError:
             raise StopIteration
         self.index += self.chunksize
@@ -186,19 +204,14 @@ class FileStream(Stream):
 
 
 def read(fname, **kwargs):
-        """ input: the filename of a raw binary file
+    """ input: the filename of a raw binary file
 
         should have an associated .meta file
         
         (should be a method of the SampledData object
         eventually)
         """
-        bark_obj = bark.read_sampled(fname)
-        data = bark_obj.data
-        sr = bark_obj.attrs["sampling_rate"]
-        return FileStream(data, sr,
-                data.attrs, **kwargs)
-
-
-
-
+    bark_obj = bark.read_sampled(fname)
+    data = bark_obj.data
+    sr = bark_obj.attrs["sampling_rate"]
+    return FileStream(data, sr, data.attrs, **kwargs)
--- tests/test_datutils.py
+++ tests/test_datutils.py
@@ -2,11 +2,12 @@ import pytest
 from bark.datutils import Stream
 import numpy as np
 
-data1 = np.arange(30).reshape(10,3)
+data1 = np.arange(30).reshape(10, 3)
 data2 = np.arange(500).reshape(100, 5)
 data3 = np.arange(1000).reshape(500, 2)
-data4 = np.arange(11111).reshape(-1,1)
+data4 = np.arange(11111).reshape(-1, 1)
 dummyf = lambda x: x
+
 
 def test_stream():
     stream = Stream(data1, sr=10)
@@ -23,7 +24,8 @@ def test_stream_chunksize():
 def test_stream_add():
     stream = Stream(data1, sr=10, chunksize=3)
     stream2 = stream + 4
-    assert np.allclose(data1+ 4, stream2.call())
+    assert np.allclose(data1 + 4, stream2.call())
+
 
 def test_stream_subtract():
     stream = Stream(data1, sr=10, chunksize=3)
@@ -53,24 +55,69 @@ def test_stream_chaining():
     stream = Stream(data1, sr=10, chunksize=3)
     stream2 = stream + 4
     stream3 = stream2 + 3
-    assert np.allclose(data1 + 7, stream3.call()) 
+    assert np.allclose(data1 + 7, stream3.call())
 
 
 def test_map():
     assert np.allclose(data1, Stream(data1, sr=1).map(dummyf).call())
-    assert np.allclose(data1 + 1, Stream(data1, sr=1).map(lambda x: x + 1).call())
+    assert np.allclose(data1 + 1,
+                       Stream(data1,
+                              sr=1).map(lambda x: x + 1).call())
     for data in (data1, data2, data3, data4):
         assert np.allclose(data, Stream(data, sr=1).map(dummyf).call())
 
+
 def test_reduce_columns_by_map():
-    myf = lambda x: np.mean(x, 1).reshape(-1,1)
-    assert np.allclose(myf(data1),
-            Stream(data1, sr=1).map(myf).call())
+    myf = lambda x: np.mean(x, 1).reshape(-1, 1)
+    assert np.allclose(myf(data1), Stream(data1, sr=1).map(myf).call())
 
 
 def test_vector_map():
     for data in (data1, data2, data3, data4):
-        y  =  Stream(data, sr=1).vector_map(dummyf).call()
+        y = Stream(data, sr=1).vector_map(dummyf).call()
         assert np.allclose(data, y)
 
 
+def test_getitem():
+    y = Stream(data1, sr=1)[-1].call()
+    assert np.allclose(data1[:, -1], y)
+    y = Stream(data1, sr=1)[-1].call()
+    assert np.allclose(data1[:, -1], y)
+
+
+def test_split():
+    y = Stream(data2, sr=1).split(1, 3).call()
+    assert np.allclose(data2[:, (1, 3)], y)
+
+
+def test_rechunk():
+    assert np.allclose(data1,
+                       Stream(data1,
+                              sr=1,
+                              chunksize=10).rechunk(11).call())
+    assert np.allclose(data1,
+                       Stream(data1,
+                              sr=1,
+                              chunksize=12).rechunk(11).call())
+
+
+def test_rechunk2():
+    assert np.allclose(data3,
+                       Stream(data3,
+                              sr=1,
+                              chunksize=12).rechunk(1).call())
+    assert np.allclose(data3,
+                       Stream(data3,
+                              sr=1,
+                              chunksize=12).rechunk(1000000).call())
+
+
+def test_merge():
+
+    assert np.allclose(
+        np.hstack((data1, data1)),
+        Stream(data1,
+               sr=1,
+               chunksize=10).merge(Stream(data1,
+                                          sr=1,
+                                          chunksize=10)).call())
