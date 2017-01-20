d7be08e02c78139a8bcfb1fcb1f3ccaff250ebfa
Graham Fetterman <graham.fetterman@gmail.com>
2017-01-19 18:15:55 -0600

    Fix tests after barksplit refactor

--- tests/test_barksplit.py
+++ tests/test_barksplit.py
@@ -24,9 +24,9 @@ def test_time_to_index():
     assert output['name'] == series['name']
 
 def test_points_to_span():
-    attrs = {'n_channels': 3, 'n_samples': 31}
-    def common(pts, attrs):
-        output = bs.points_to_span(pts, attrs)
+    n_samples = 31
+    def common(pts, ns):
+        output = bs.points_to_span(pts, ns)
         assert isinstance(output, pd.DataFrame)
         assert 'start' in output
         assert 'stop' in output
@@ -34,21 +34,21 @@ def test_points_to_span():
         assert len(output) == 5
         assert output['start'][0] == 0
         assert output['start'][1] == 7
-        assert output['stop'][4] == 93
-        assert output['stop'][3] == 89
+        assert output['stop'][4] == 31
+        assert output['stop'][3] == 29
         assert output['name'][0] == ''
     # points includes neither start nor end of interval
-    points = [7, 19, 53, 89]
-    common(points, attrs)
+    points = [7, 19, 25, 29]
+    common(points, n_samples)
     # points includes start of interval
-    points = [0, 7, 19, 53, 89]
-    common(points, attrs)
+    points = [0, 7, 19, 25, 29]
+    common(points, n_samples)
     # points includes end of interval
-    points = [7, 19, 53, 89, 93]
-    common(points, attrs)
+    points = [7, 19, 25, 29, 31]
+    common(points, n_samples)
     # points includes both start and end of interval
-    points = [0, 7, 19, 53, 89, 93]
-    common(points, attrs)
+    points = [0, 7, 19, 25, 29, 31]
+    common(points, n_samples)
 
 def test_label_to_splits():
     try:
@@ -67,8 +67,10 @@ def test_label_to_splits():
     sdata = np.array(random.sample(xrange(100), 93)).reshape(-1, 3)
     sattrs = {'sampling_rate': 13, 'n_samples': 31, 'n_channels': 3}
     samp = bark.SampledData(sdata, path, sattrs)
-    edata = pd.DataFrame({'start': [0.54, 1.46, 4.08, 6.85],
-                              'name': ['a', 'b', 'a', 'c']})
+    start_samples = [7, 19, 25, 29]
+    start_times = [x / sattrs['sampling_rate'] for x in start_samples]
+    edata = pd.DataFrame({'start': start_times,
+                          'name': ['a', 'b', 'a', 'c']})
     eattrs = {'units': 's'}
     event = bark.EventData(edata, path, eattrs)
     # point mode, split_on empty
@@ -78,11 +80,12 @@ def test_label_to_splits():
     assert len(output['start']) == 5
     assert output['start'][0] == 0
     assert output['start'][1] == 7
-    assert output['stop'][4] == 93
-    assert output['stop'][3] == 89
+    assert output['stop'][4] == 31
+    assert output['stop'][3] == 29
     assert output['name'][0] == ''
     # interval mode, split_on empty
-    edata['stop'] = [.77, 2.31, 6.15, 6.92]
+    stop_samples = [10, 20, 28, 31]
+    edata['stop'] = [x / sattrs['sampling_rate'] for x in stop_samples]
     event = bark.EventData(edata, path, eattrs)
     output = bs.label_to_splits(samp, event, split_on, point_mode=False)
     common(output)
@@ -91,7 +94,7 @@ def test_label_to_splits():
     assert output['stop'][0] == 10
     assert output['name'][0] == event.data['name'][0]
     assert output['start'][1] == 19
-    assert output['stop'][1] == 30
+    assert output['stop'][1] == 20
     assert output['name'][1] == event.data['name'][1]
     # interval mode, split_on given
     split_on = 'a'
@@ -101,8 +104,8 @@ def test_label_to_splits():
     assert output['start'][0] == 7
     assert output['stop'][0] == 10
     assert output['name'][0] == event.data['name'][0]
-    assert output['start'][1] == 53
-    assert output['stop'][1] == 80
+    assert output['start'][1] == 25
+    assert output['stop'][1] == 28
     assert output['name'][1] == event.data['name'][2]
 
 def test_gen_split_files(tmpdir):
