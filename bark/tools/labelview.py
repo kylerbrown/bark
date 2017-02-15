c00ca27e8d47aab1ed8b7b9c067402c1bdefd45b
Kyler Brown <kylerjbrown@gmail.com>
2017-02-14 18:22:18 -0600

    Safer save on exit, style changes
    Used a context manager to ensure a save prompt, even if error is raised. Check for units turned into function

--- bark/tools/labelview.py
+++ bark/tools/labelview.py
@@ -1,3 +1,4 @@
+import warnings
 import string
 import yaml
 import numpy as np
@@ -5,7 +6,7 @@ from scipy.signal import spectrogram
 import matplotlib.pyplot as plt
 import bark
 
-help_string = """
+help_string = '''
 Pressing any number or letter (uppercase or lowercase) will mark a segment.
 To create custom label, create an external file with key: value pairs like this:
     1: c1
@@ -27,7 +28,7 @@ The bottom panel is a map of all label locations.
 Click on a label to travel to that location.
 
 On close, you'll be prompted to save your work. Hit 'y <enter>' to save.
-"""
+'''
 
 # kill all the shorcuts
 plt.rcParams['keymap.all_axes'] = ''
@@ -105,15 +106,16 @@ def plot_spectrogram(data,
                      lowfreq=300,
                      highfreq=8000,
                      **kwargs):
-    """
+    '''
     data : a vector of samples, first sample starts at time = 0
     sr : sampling rate
     start : start time to slice data, units: seconds
     stop : stop time to slice data, units: seconds
     ms_nfft : width of fourier transform in milliseconds
-    decent colormaps: cm.viridis, cm.inferno, cm.magma, cm.plasma
-    extra arguments are sent to plt.pcolorfast
-    """
+    ax : axis object to plot spectrogram on.
+    lowfreq : lowest frequency to plot
+    highfreq : highest frequency to plot
+    '''
     nfft = int(ms_nfft / 1000. * sr)
     start_samp = int(start * sr) - nfft // 2
     stop_samp = int(stop * sr) - nfft // 2
@@ -140,9 +142,9 @@ def plot_spectrogram(data,
 
 
 class SegmentReviewer:
-    def __init__(self, figure, osc_ax, spec_ax, map_ax, sampled, labels,
+    def __init__(self, osc_ax, spec_ax, map_ax, sampled, labels,
                  shortcuts, outfile):
-        self.figure = figure
+        self.canvas = osc_ax.get_figure().canvas
         self.osc_ax = osc_ax
         self.spec_ax = spec_ax
         self.map_ax = map_ax
@@ -153,8 +155,6 @@ class SegmentReviewer:
         self.outfile = outfile
         self.shortcuts = shortcuts
         self.i = 0
-        self.label_units = labels.attrs["units"]
-        assert self.label_units == "s"
         self.N_points = 20000
         self.initialize_plots()
         self.update_plot_data()
@@ -175,7 +175,7 @@ class SegmentReviewer:
         self.osc_boundary_stop = self.osc_ax.axvline(color='r')
         self.syl_labels = [self.osc_ax.text(0,
                                             0,
-                                            "",
+                                            '',
                                             size='xx-large',
                                             color='r') for _ in range(20)]
         self.initialize_minimap()
@@ -205,7 +205,7 @@ class SegmentReviewer:
         self.map_ax.set_ylim(-1, 38)
 
     def label_nearby_syls(self):
-        "labels for current syl and two on either side"
+        'labels for current syl and two on either side'
         for i in range(-10 + self.i, 11 + self.i):
             label_i = i - self.i
             if i >= 0 and i < len(self.labels):
@@ -228,11 +228,13 @@ class SegmentReviewer:
         self.osc_boundary_stop.set_xdata((stop, stop))
 
     def update_minimap(self):
+        # If perfomance lags, may need to adjust plot elements instead of 
+        # clearing everything and starting over.
         self.map_ax.clear()
         self.initialize_minimap()
 
     def update_plot_data(self):
-        "updates plot data"
+        'updates plot data on all three axes'
         self.selected_boundary = None
         i = self.i
         sr = self.sr
@@ -256,7 +258,7 @@ class SegmentReviewer:
             self.osc_ax.set_title('ctrl+h for help, prints to terminal')
         else:
             self.osc_ax.set_title('{} of {}'.format(i + 1, len(self.labels)))
-        self.figure.canvas.draw()
+        self.canvas.draw()
 
     def update_spectrogram(self):
         self.spec_ax.clear()
@@ -274,40 +276,40 @@ class SegmentReviewer:
         self.osc_ax.set_ylim(min(x), max(x))
 
     def connect(self):
-        "creates all the event connections"
-        self.cid_key_press = self.figure.canvas.mpl_connect('key_press_event',
+        'creates all the event connections'
+        self.cid_key_press = self.canvas.mpl_connect('key_press_event',
                                                             self.on_key_press)
-        self.cid_mouse_press = self.figure.canvas.mpl_connect(
+        self.cid_mouse_press = self.canvas.mpl_connect(
             'button_press_event', self.on_mouse_press)
-        self.cid_mouse_release = self.figure.canvas.mpl_connect(
+        self.cid_mouse_release = self.canvas.mpl_connect(
             'button_release_event', self.on_mouse_release)
 
     def on_mouse_press(self, event):
-        if event.inaxes is None:
+        if event.inaxes in (None, self.spec_ax) or event.button != 1:
             return
-        if event.button != 1:
-            return
-        xlim1, xlim2 = self.osc_ax.get_xlim()
-        xspan = xlim2 - xlim1
         start_pos = self.osc_boundary_start.get_xdata()[0]
         stop_pos = self.osc_boundary_stop.get_xdata()[0]
+        # jump to syllable from map click
         if event.inaxes == self.map_ax:
             i = nearest_label(self.labels, event.xdata)
             self.i = i
             self.update_plot_data()
         # sylable splitting
-        if event.key == 'control' and event.xdata > start_pos and event.xdata < stop_pos:
+        elif (event.key == 'control' and event.xdata > start_pos and
+              event.xdata < stop_pos):
             seg_split(self.labels, self.i, event.xdata)
             self.update_plot_data()
         # boundary updates
         else:
+            xlim1, xlim2 = self.osc_ax.get_xlim()
+            xspan = xlim2 - xlim1
             if abs(event.xdata - start_pos) / xspan < 0.006:
                 self.selected_boundary = self.osc_boundary_start
             elif abs(event.xdata - stop_pos) / xspan < 0.006:
                 self.selected_boundary = self.osc_boundary_stop
             if self.selected_boundary:
                 self.selected_boundary.set_color('y')
-                self.figure.canvas.draw()
+                self.canvas.draw()
 
     def on_mouse_release(self, event):
         if self.selected_boundary == self.osc_boundary_start:
@@ -318,16 +320,16 @@ class SegmentReviewer:
             self.selected_boundary.set_color('r')
             self.update_syl_boundaries()
         self.selected_boundary = None
-        self.figure.canvas.draw()
+        self.canvas.draw()
 
     def inc_i(self):
-        "Go to next syllable."
+        'Go to next syllable.'
         if self.i < len(self.labels) - 1:
             self.i += 1
         self.update_plot_data()
 
     def dec_i(self):
-        "Go to previous syllable."
+        'Go to previous syllable.'
         if self.i > 0:
             self.i -= 1
         self.update_plot_data()
@@ -361,11 +363,19 @@ class SegmentReviewer:
             self.update_plot_data()
 
     def save(self):
-        "Writes out labels to file."
+        'Writes out labels to file.'
         import pandas as pd
         label_data = pd.DataFrame(self.labels)
         bark.write_events(self.outfile, label_data, **self.label_attrs)
-        print(self.outfile, "written")
+        print(self.outfile, 'written')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        response = input('Save to file {}? '.format(self.outfile))
+        if response.lower() in ('y', 'yes', 'obvs bro'):
+            self.save()
 
 
 def build_shortcut_map(mapfile=None):
@@ -375,51 +385,64 @@ def build_shortcut_map(mapfile=None):
     if mapfile:
         custom = {str(key): value
                   for key, value in yaml.load(open(mapfile, 'r')).items()}
-        print("custom keymaps:", custom)
+        print('custom keymaps:', custom)
         shortcut_map.update(custom)
     return shortcut_map
+
+
+def to_seconds(dset):
+    'TODO Converts bark EventData object to units of seconds.'
+    if 'offset' in dset.attrs and dset.attrs['offset'] != 0:
+        raise Exception('offsets are not yet supported in event file')
+    if dset.attrs['units'] != 's':
+        raise Exception('only units of s are supported in event file')
+    return dset
 
 
 def main(datfile, labelfile, outfile, shortcutfile=None):
     sampled = bark.read_sampled(datfile)
     assert sampled.attrs['n_channels'] == 1
     labels = bark.read_events(labelfile)
+    labes = to_seconds(labels)
     shortcuts = build_shortcut_map(shortcutfile)
-    #f, (osc_ax, spec_ax) = plt.subplots(2, 1, sharex=True)
     f = plt.figure()
+    # Oscillogram and Spectrogram get 
+    # three times the vertical space as the minimap.
     osc_ax = plt.subplot2grid((7, 1), (0, 0), rowspan=3)
     spec_ax = plt.subplot2grid((7, 1), (3, 0), rowspan=3, sharex=osc_ax)
     map_ax = plt.subplot2grid((7, 1), (6, 0))
-    f.tight_layout()
-    reviewer = SegmentReviewer(f, osc_ax, spec_ax, map_ax, sampled, labels,
-                               shortcuts, outfile)
-    reviewer.connect()
-    plt.show()
-    response = input("Save to file {}? ".format(outfile))
-    if response.lower() in ('y', 'yes', 'obvs bro'):
-        reviewer.save()
+    # Segement review is a context manager to ensure a save prompt
+    # on exit. see SegmentReviewer.__exit__
+    with SegmentReviewer(osc_ax, spec_ax, map_ax, sampled, labels,
+                         shortcuts, outfile) as reviewer:
+        reviewer.connect()
+        plt.show()
 
 
 def _run():
     import argparse
 
-    p = argparse.ArgumentParser(description="""
+    p = argparse.ArgumentParser(description='''
     Review and annotate segments
-    """)
-    p.add_argument("dat", help="name of a sampled dataset")
-    p.add_argument("-l",
-                   "--labelfile",
+    ''')
+    p.add_argument('dat', help='name of a sampled dataset')
+    p.add_argument('-l',
+                   '--labelfile',
                    required=True,
-                   help="associated event dataset containing segments")
-    p.add_argument("-o", "--out", help="output label file", required=True)
-    p.add_argument("-k",
-                   "--shortcut-keys",
-                   help="""YAML file with keyboard shortcuts.
-        Keys are digits, lowercase and uppercase characters""")
+                   help='associated event dataset containing segments')
+    p.add_argument('-o', '--out', help='output label file', required=True)
+    p.add_argument('-k',
+                   '--shortcut-keys',
+                   help='''YAML file with keyboard shortcuts.
+        Keys are digits, lowercase and uppercase characters''')
 
     args = p.parse_args()
     main(args.dat, args.labelfile, args.out, args.shortcut_keys)
 
 
-if __name__ == "__main__":
-    _run()
+if __name__ == '__main__':
+    # The spectrogram function prints a harmless warning.
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore',
+                                r'overflow encountered in short_scalars')
+        _run()
