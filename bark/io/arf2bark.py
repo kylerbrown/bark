5fe9312451947a9fd2ce318524ee57a445d2cfed
Graham Fetterman <graham.fetterman@gmail.com>
2017-03-24 16:16:38 -0500

    Produce informative msgs for trig_in & jill_log

--- bark/io/arf2bark.py
+++ bark/io/arf2bark.py
@@ -38,6 +38,7 @@ def arf2bark(arf_file, root_parent, verbose):
         if verbose:
             print('Created Root: ' + root_path)
         tle = None
+        found_trigin = False
         for ename, entry in af.items(): # entries and top-level datasets
             if isinstance(entry, h5py.Group): # entries
                 entry_path = os.path.join(root_path, ename)
@@ -50,39 +51,45 @@ def arf2bark(arf_file, root_parent, verbose):
                 if verbose:
                     print('Created Entry: ' + entry_path)
                 for ds_name, dataset in entry.items(): # entry-level datasets
-                    if ds_name != 'trig_in': # accessing trig_in -> segfault
+                    if ds_name == 'trig_in': # accessing trig_in -> segfault
+                        found_trigin = True # and skip the dataset
+                    else:
                         handle_dset(ds_name, dataset, entry_path, verbose)
             elif isinstance(entry, h5py.Dataset): # top-level datasets
-                if ename != 'jill_log': # jill_log: neither pp nor timeseries??
-                    if tle is None:
-                        path = os.path.join(root_path, 'top_level')
-                        tle = bark.create_entry(path, 0, parents=False).path
-                    handle_dset(ename, entry, tle, verbose)
+                if tle is None:
+                    path = os.path.join(root_path, 'top_level')
+                    tle = bark.create_entry(path, 0, parents=False).path
+                handle_dset(ename, entry, tle, verbose)
+        if found_trigin:
+            print('Warning: found datasets named "trig_in". Jill-created ' +
+                  '"trig_in" datasets segfault when read, so these datasets' +
+                  ' were skipped. If you know the datasets are good, rename' +
+                  ' them and try again.')
     return bark.Root(root_path)
 
-def handle_dset(ds_name, ds, entry_path, verbose=False):
+def handle_dset(ds_name, ds, e_path, verbose=False):
     ds_attrs = copy_attrs(ds.attrs)
-    units = ds_attrs.pop('units')
+    units = ds_attrs.pop('units', None)
     if units == '':
         units = None
     ds_attrs['columns'] = {0: {'units': units}}
     if arf.is_time_series(ds):
         ds_name += '.dat'
-        ds_path = os.path.join(entry_path, ds_name)
+        ds_path = os.path.join(e_path, ds_name)
         sr = ds_attrs.pop('sampling_rate')
         bark_ds = bark.write_sampled(ds_path, ds, sr, **ds_attrs)
         if verbose:
             print('Created sampled dataset: ' + ds_path)
     elif arf.is_marked_pointproc(ds):
         ds_name += '.csv'
-        ds_path = os.path.join(entry_path, ds_name)
+        ds_path = os.path.join(e_path, ds_name)
         ds_data = pandas.DataFrame(ds)
         bark_ds = bark.write_events(ds_path, ds_data, **ds_attrs)
         if verbose:
             print('Created event dataset: ' + ds_path)
     else:
-        msg = 'Unknown dataset type - neither time series nor point process.'
-        raise ValueError(msg)
+        print('Warning: unknown dataset type - neither time series nor point' +
+              ' process. Skipping dataset ' + ds_name)
 
 def _main():
     arg_dict = _parse_args(sys.argv[1:])
