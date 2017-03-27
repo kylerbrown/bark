25fc4cee72f93585f503450f5cea81b45c58efcc
Graham Fetterman <gfetterman@users.noreply.github.com>
2017-03-27 15:11:16 -0500

    fix event dataset handling in arf2bark (#31)

--- bark/io/arf2bark.py
+++ bark/io/arf2bark.py
@@ -5,6 +5,8 @@ import os
 import bark
 import h5py
 import pandas
+import numpy
+import collections as coll
 
 def _parse_args(raw_args):
     desc = 'Unspool an HDF5 ARF file into a Bark tree.'
@@ -19,14 +21,10 @@ def _parse_args(raw_args):
 
 def copy_attrs(attrs):
     """Semi-deep-copies an h5py attributes dictionary,
-       because h5py isn't smart enough to do it right."""
-    new_attrs = {}
-    for a in attrs:
-        if isinstance(attrs[a], bytes):
-            new_attrs[a] = attrs[a].decode()
-        else:
-            new_attrs[a] = attrs[a]
-    return new_attrs
+       and converts byte-strings to strings."""
+    na = dict(attrs)
+    na.update({k: v.decode() for k,v in na.items() if isinstance(v, bytes)})
+    return na
 
 def arf2bark(arf_file, root_parent, verbose):
     with arf.open_file(arf_file, 'r') as af:
@@ -54,12 +52,12 @@ def arf2bark(arf_file, root_parent, verbose):
                     if ds_name == 'trig_in': # accessing trig_in -> segfault
                         found_trigin = True # and skip the dataset
                     else:
-                        handle_dset(ds_name, dataset, entry_path, verbose)
+                        transfer_dset(ds_name, dataset, entry_path, verbose)
             elif isinstance(entry, h5py.Dataset): # top-level datasets
                 if tle is None:
                     path = os.path.join(root_path, 'top_level')
                     tle = bark.create_entry(path, 0, parents=False).path
-                handle_dset(ename, entry, tle, verbose)
+                transfer_dset(ename, entry, tle, verbose)
         if found_trigin:
             print('Warning: found datasets named "trig_in". Jill-created ' +
                   '"trig_in" datasets segfault when read, so these datasets' +
@@ -67,15 +65,24 @@ def arf2bark(arf_file, root_parent, verbose):
                   ' them and try again.')
     return bark.Root(root_path)
 
-def handle_dset(ds_name, ds, e_path, verbose=False):
+def build_columns(units, column_names=None):
+    if isinstance(units, (str, bytes)) or not isinstance(units, coll.Iterable):
+        units = [units]
+    if column_names is None:
+        column_names = range(len(units))
+    cols = {cn: {'units': u} for cn,u in zip(column_names, units)}
+    for c,d in cols.items():
+        d.update({k: v.decode() for k,v in d.items() if isinstance(v, bytes)})
+        d.update({k: None for k,v in d.items() if (k == 'units' and v == '')})
+    return cols
+
+def transfer_dset(ds_name, ds, e_path, verbose=False):
     ds_attrs = copy_attrs(ds.attrs)
     units = ds_attrs.pop('units', None)
-    if units == '':
-        units = None
-    ds_attrs['columns'] = {0: {'units': units}}
     if arf.is_time_series(ds):
         ds_name += '.dat'
         ds_path = os.path.join(e_path, ds_name)
+        ds_attrs['columns'] = build_columns(units)
         sr = ds_attrs.pop('sampling_rate')
         bark_ds = bark.write_sampled(ds_path, ds, sr, **ds_attrs)
         if verbose:
@@ -83,7 +90,11 @@ def handle_dset(ds_name, ds, e_path, verbose=False):
     elif arf.is_marked_pointproc(ds):
         ds_name += '.csv'
         ds_path = os.path.join(e_path, ds_name)
-        ds_data = pandas.DataFrame(ds)
+        ds_data = pandas.DataFrame(ds[:])
+        ds_attrs['columns'] = build_columns(units, column_names=ds_data.columns)
+        for ser in ds_data:
+            if ds_data[ser].dtype == numpy.dtype('O'): # bytes object
+                ds_data[ser] = ds_data[ser].str.decode('utf-8')
         bark_ds = bark.write_events(ds_path, ds_data, **ds_attrs)
         if verbose:
             print('Created event dataset: ' + ds_path)
